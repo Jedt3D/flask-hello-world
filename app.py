b6956c1558261a7b1f02079d303f47c22cc8114d
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2023-08-07 02:25:10 +0700

    เพ่ิมการแสดงเลขเวอร์ชั่น

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import platform
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return '<code>Hello, New World!</code>'
+    version = platform.python_version()
+    return '<code>Hello, New World!<br>We\'re using Python ' + version + '</code>'
